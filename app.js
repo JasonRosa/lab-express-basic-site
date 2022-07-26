4909d7cab9b2c9937bc65fd5e79dea6cd4a41981
JasonRosa <105085467+JasonRosa@users.noreply.github.com>
2022-07-26 18:12:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//const { response } = require('express');
+const express = require('express');
+const app = express();
+app.use(express.static('Public'));
+
+app.get('/home', (req, res, next) => {
+    res.sendFile(__dirname + '/public/views/home.html');
+  });
+  
+  app.get('/about', (req, res, next) => {
+    console.log('About')
+    res.sendFile(__dirname + '/public/views/about.html');
+
+  });
+  
+  app.get('/works', (req, res, next) => {
+    console.log(req);
+    res.sendFile(__dirname + '/public/views/works.html');
+  });
+
+  app.get('/cv', (req, res, next) => {
+    console.log(req);
+    res.sendFile(__dirname + '/public/views/cv.html');
+  });
+
+  app.get('/contact', (req, res, next) => {
+    console.log(req);
+    res.sendFile(__dirname + '/public/views/contact.html');
+  });
+
+
+  app.get('/Gallery', (req, res, next) => {
+    console.log(req);
+    res.sendFile(__dirname + '/public/views/Gallery.html');
+  });
+  
+  app.listen(3000, () => {
+    console.log('Running on port 3000');
+  });
